637785acd45eeb8ab6cca441442c43bf7b1a2b48
Justin Paul <jspaul94@gmail.com>
2014-03-12 22:33:35 -0500

    First commit

---
+++ UDPGroupChat/src/udpgroupchat/server/WorkerThread.java
@@ -0,0 +1,186 @@
+package udpgroupchat.server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class WorkerThread extends Thread {
+
+	private DatagramPacket rxPacket;
+	private DatagramSocket socket;
+
+	public WorkerThread(DatagramPacket packet, DatagramSocket socket) {
+		this.rxPacket = packet;
+		this.socket = socket;
+	}
+
+	@Override
+	public void run() {
+		// convert the rxPacket's payload to a string
+		String payload = new String(rxPacket.getData(), 0, rxPacket.getLength())
+				.trim();
+
+		// dispatch request handler functions based on the payload's prefix
+
+		if (payload.startsWith("REGISTER")) {
+			onRegisterRequested(payload);
+			return;
+		}
+
+		if (payload.startsWith("UNREGISTER")) {
+			onUnregisterRequested(payload);
+			return;
+		}
+
+		if (payload.startsWith("SEND")) {
+			onSendRequested(payload);
+			return;
+		}
+		
+		if (payload.startsWith("JOIN")) {
+			onJoinRequested(payload);
+			return;
+		}
+		
+		if (payload.startsWith("POLL")) {
+			onPollRequested(payload);
+			return;
+		}
+		
+		if (payload.startsWith("IPCHANGE")) {
+			onIPChangeRequested(payload);
+			return;
+		}
+		
+		if (payload.startsWith("SHUTDOWN")) {
+			onShutdownRequested(payload);
+			return;
+		}
+
+		//
+		// implement other request handlers here...
+		//
+
+		// if we got here, it must have been a bad request, so we tell the
+		// client about it
+		onBadRequest(payload);
+	}
+
+	// send a string, wrapped in a UDP packet, to the specified remote endpoint
+	public void send(String payload, InetAddress address, int port)
+			throws IOException {
+		DatagramPacket txPacket = new DatagramPacket(payload.getBytes(),
+				payload.length(), address, port);
+		this.socket.send(txPacket);
+	}
+
+	private void onRegisterRequested(String payload) {
+		// get the address of the sender from the rxPacket
+		InetAddress address = this.rxPacket.getAddress();
+		// get the port of the sender from the rxPacket
+		int port = this.rxPacket.getPort();
+
+		// create a client object, and put it in the map that assigns names
+		// to client objects
+		Server.clientEndPoints.add(new ClientEndPoint(address, port));
+		// note that calling clientEndPoints.add() with the same endpoint info
+		// (address and port)
+		// multiple times will not add multiple instances of ClientEndPoint to
+		// the set, because ClientEndPoint.hashCode() is overridden. See
+		// http://docs.oracle.com/javase/7/docs/api/java/util/Set.html for
+		// details.
+
+		// tell client we're OK
+		try {
+			send("REGISTERED\n", this.rxPacket.getAddress(),
+					this.rxPacket.getPort());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void onUnregisterRequested(String payload) {
+		ClientEndPoint clientEndPoint = new ClientEndPoint(
+				this.rxPacket.getAddress(), this.rxPacket.getPort());
+
+		// check if client is in the set of registered clientEndPoints
+		if (Server.clientEndPoints.contains(clientEndPoint)) {
+			// yes, remove it
+			Server.clientEndPoints.remove(clientEndPoint);
+			try {
+				send("UNREGISTERED\n", this.rxPacket.getAddress(),
+						this.rxPacket.getPort());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			// no, send back a message
+			try {
+				send("CLIENT NOT REGISTERED\n", this.rxPacket.getAddress(),
+						this.rxPacket.getPort());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void onSendRequested(String payload) {
+		// the message is comes after "SEND" in the payload
+		String message = payload.substring("SEND".length() + 1,
+				payload.length()).trim();
+		for (ClientEndPoint clientEndPoint : Server.clientEndPoints) {
+			try {
+				send("MESSAGE: " + message + "\n", clientEndPoint.address,
+						clientEndPoint.port);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void onJoinRequested(String payload) {
+		// the message is comes after "JOIN" in the payload
+		//QUESITONS:
+		//1. Should we have a Hashmap of group name to list of people in group?
+		//2. Thus should we edit Register to register to a certain group and
+		//   send to send to people in the certain group?
+	}
+	
+	private void onPollRequested(String payload) {
+		// the message is comes after "POLL" in the payload
+		//QUESTIONS REGARDING POLL:
+		//1. I assume we will need a HashMap for messages waiting to be sent
+		//   How do we keep track of which messages need to be sent to which people?
+		
+	}
+	
+	private void onIPChangeRequested(String payload) {
+		// the message is comes after "IPChange" in the payload
+		//QUESTIONS REGARDING POLL:
+		//1. This seems like to do this we should change its values in on the HashMaps?
+		//2. Should I have them have arguments pass for this such as
+		//   1. New IP Address
+		//   2. Old IP Address as proof of correct client?
+		
+	}
+	
+	private void onShutdownRequested(String payload) {
+		// the message is comes after "Shutdown" in the payload
+		//QUESTIONS REGARDING POLL:
+		//1. If shutdown is sent, should we unregister everyone?
+		//2. If Shutdown is sent, should it be similar as to the shutdown in the previous project?
+		
+	}
+
+
+	private void onBadRequest(String payload) {
+		try {
+			send("BAD REQUEST\n", this.rxPacket.getAddress(),
+					this.rxPacket.getPort());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
